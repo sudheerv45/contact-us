7ca3010517c4dc5d0764125d6189e58e24418fd7
sudheer45 <sudheervannam123@gmail.com>
2024-07-09 15:49:44 +0530

    first commit

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const needHelpRoutes = require('./routes/needHelpRoutes');
+const cors = require('cors');
 
 const app = express();
+
+
+
+
+// Enable All CORS Requests
+app.use(cors());
+
+app.use(cors({
+    origin: '*' // Allow only requests from this origin
+}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // Connect Database
 connectDB();
